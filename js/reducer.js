01ebde5f7fcab60b2f78d6c80c1f8d7136378523
dangrammer <danieljromans@gmail.com>
2020-01-16 12:40:11 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,27 @@
-// add code snippets from README
+const button = document.querySelector('#button')
+const countDiv = document.querySelector('#container')
+let state
+
+function reducer(state = {count: 0}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state
+  }
+}
+
+function render() {
+  countDiv.innerText = state.count
+}
+
+function dispatch(action) {
+  state = reducer(state, action)
+  render()
+}
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+dispatch({type: '@@INIT'})
